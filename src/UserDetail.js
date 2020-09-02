4f456a387a0ba46b29aed45b3c6c89f8bc869ec8
sangjun <juntech0421@github.com>
2020-09-02 15:59:21 +0900

    UserDetail.js 주석처리

--- src/UserDetail.js
+++ src/UserDetail.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useAsync } from "react-async";
 import axios from "axios";
 
+//axios로 api받아오고 백틱을 사용해 id에 따른 url 요청 비동기처리
 async function getUser({ id }) {
   const response = await axios.get(
     `https://jsonplaceholder.typicode.com/users/${id}`
@@ -9,8 +10,12 @@ async function getUser({ id }) {
   return response.data;
 }
 
+//비구조 할당으로 id props로 받아옴. 
 function UserDetail({ id }) {
-  const { data: user, error, isLoading } = useAsync({
+    /* useAsync(react-async)활용 비구조할당으로 받아오고
+    비동기함수는 getUser watch는 deps와 같은역할이므로
+    id값이 변할 때 마다 리렌더링 */
+    const { data: user, error, isLoading } = useAsync({
     promiseFn: getUser,
     id,
     watch: id,
